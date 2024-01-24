604640b1c7762f77177e278a529fbce68d728fdd
Shoinbek <s.shoinbekov@gmail.com>
2024-01-24 12:44:18 +0800

    first commit

---
+++ Data_Types.py
@@ -0,0 +1,18 @@
+#exercise 1:
+x = 5
+print(type(x))
+#int
+
+#exercise 2:
+x = "Hello World"
+print(type(x))
+#str
+
+#exercise 3:
+x = 20.5
+print(type(x))
+#float
+
+#exercise 4:
+x = ["apple", "banana", "cherry"]
+print(type(x))
