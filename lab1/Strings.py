21511256e8a5e92e2398e5c87df32e96413966cd
Shoinbek <s.shoinbekov@gmail.com>
2024-01-25 21:30:29 +0800

    first commit

---
+++ lab1/Strings.py
@@ -0,0 +1,37 @@
+#exercise 1:
+x = "Hello World"
+print(len(x))
+
+#exercise 2:
+txt = "Hello World"
+x = txt[0]
+print(x)
+
+#exercise 3:
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+#exercise 4:
+txt = " Hello World "
+print(txt.strip())
+
+#exercise 5:
+txt = "Hello World"
+txt = txt.upper()
+print(txt)
+
+#exercise 6:
+txt = "Hello World"
+txt = txt.lower()
+print(txt)
+
+#exercie 7:
+txt = "Hello World"
+txt = txt.replace("H", "J")
+print(txt)
+
+#exercise 8:
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
