08822625efaade1f0c98f20298294d16e5c21bd7
Shoinbek <s.shoinbekov@gmail.com>
2024-01-28 22:54:08 +0800

    second commit

--- lab2/Boolean.py
+++ lab2/Boolean.py
@@ -1,5 +1,14 @@
-x = "Hello"
-y = 15
+#exercise 1:
+print(10 > 9)
 
-print(bool(x))
-print(bool(y))
+#exercise 2:
+print(10 == 9)
+
+#exercise 3:
+print(10 < 9)
+
+#exercise 4:
+print(bool("abc"))
+
+#exercise 5:
+print(bool(0))
