179839116451de5fedbc8a2f3b572917c4897b4d
Shoinbek <s.shoinbekov@gmail.com>
2024-03-07 01:03:50 +0800

    first commit

--- lab4/scope.py
+++ lab4/scope.py
@@ -4,3 +4,32 @@ def myfunc():
   print(x)
 
 myfunc()
+
+#exercise 2:
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+#exercise 3:
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x)
+
+#exercise 4:
+x = 300
+
+def myfunc():
+  x = 200
+  print(x)
+
+myfunc(
+)
